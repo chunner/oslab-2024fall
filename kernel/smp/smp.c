18c065f10966cad56798f43caa4b7f46f2455b52
aihuachun <aihuachun22@mails.ucas.ac.cn>
2024-10-28 10:29:52 +0000

    fix(smp): use the swap

--- kernel/smp/smp.c
+++ kernel/smp/smp.c
@@ -4,22 +4,9 @@
 #include <os/lock.h>
 #include <os/kernel.h>
 
-typedef volatile uint64_t spinlock_t;
+typedef volatile uint32_t spinlock_t;
 spinlock_t hart_lock;
 
-
-// void hart_lock_acquire() {
-//     // while (atomic_cmpxchg(0, 1, (ptr_t) &hart_lock) != 0) {
-//     // }
-//     while (atomic_swap(1, &hart_lock) != 0) {
-
-//     }
-//     return;
-// }
-
-// void hart_lock_release() {
-//     hart_lock = 0;
-// }
 
 void smp_init()
 {
@@ -40,30 +27,33 @@ void wakeup_other_hart()
 void lock_kernel()
 {
     /* TODO: P3-TASK3 multicore*/
-    // while (atomic_swap_d(1, &hart_lock) != 0) {
+    while (atomic_swap(1, &hart_lock) != 0) {
 
-    // }
-    asm volatile(
-        "1: li t0, 1\n"
-        "   amoswap.w.aq t1, t0, (%0)\n"
-        "   bnez t1, 1b\n"
-        :           // no output
-    : "r" (&hart_lock)  // input
-        : "t0", "t1"    // changed
-        );
+    }
+    //printl("ok\n");
+    // asm volatile(
+    //     "1: li t0, 1\n"
+    //     "   amoswap.w.aq t1, t0, (%0)\n"
+    //     "   bnez t1, 1b\n"
+    //     :           // no output
+    // : "r" (&hart_lock)  // input
+    //     : "t0", "t1"    // changed
+    //     );
     return;
 }
 
 void unlock_kernel()
 {
     /* TODO: P3-TASK3 multicore*/
-    // hart_lock = 0;
-    asm volatile(
-        "   li t0, 0\n"
-        "   amoswap.w.rl x0, t0, (%0)\n"
-        :
-    : "r" (&hart_lock)
-        : "t0"
-        );
+    //printl("ulock\n");
+    hart_lock = 0;
+    //atomic_swap(0, &hart_lock);
+    // asm volatile(
+    //     "   li t0, 0\n"
+    //     "   amoswap.w.rl x0, t0, (%0)\n"
+    //     :
+    // : "r" (&hart_lock)
+    //     : "t0"
+    //     );
     return;
 }
