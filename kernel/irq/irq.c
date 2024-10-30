8a2fa473630a8073edde04bad96dd5c76c335232
aihuachun <aihuachun22@mails.ucas.ac.cn>
2024-10-30 08:46:35 +0000

    fix: modify kill and exit

--- kernel/irq/irq.c
+++ kernel/irq/irq.c
@@ -19,6 +19,9 @@ void interrupt_helper(regs_context_t *regs, uint64_t stval, uint64_t scause)
     // get_current_running();
     // TODO: [p2-task3] & [p2-task4] interrupt handler.
     // call corresponding handler by the value of `scause`
+    if (current_running->pcb_status == PCB_INACTIVE || current_running->status == TASK_EXITED) {
+        do_exit();
+    }
     if (scause & SCAUSE_IRQ_FLAG) {       // Interrupt = 1
         irq_table[scause & SCAUSE_EXC_CODE](regs, stval, scause);
     } else {            // Interrupt = 0
