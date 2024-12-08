e5314af4217a4fe0029c837dacf550c9386ee0f0
aihuachun <aihuachun22@mails.ucas.ac.cn>
2024-12-08 08:12:37 +0000

    feat(send1): modify packet sending loop to use sys_net_multisend for improved efficiency

--- test/test_project5/send1.c
+++ test/test_project5/send1.c
@@ -34,11 +34,12 @@ int main(void)
     *(char *) ((char *) buffer + sizeof(eh)) = 43;   // magic num
     sys_move_cursor(0, 5);
     printf("> [SEND1] start send package.               \n");
-    for (int i = 0;i < 1024;i++) {
+    for (int i = 0;i < 16;i++) {
         sys_move_cursor(0, 6);
-        sys_net_send(buffer, len);        // send 1 packet one time
+        // sys_net_send(buffer, len);        // send 1 packet one time
+        sys_net_multisend(buffer, 64, len);  // send 1 packet 64 times
         uint32_t checksum = adler32((char *) buffer, len);
-        printf("> [SEND1] totally send %d   KB !         \n", (i + 1) * len / 1024);
+        printf("> [SEND1] totally send %d   KB !         \n", (i + 1) * len * 64 / 1024);
         printf("> [SEND1] checksum: %x\n", checksum);
     }
 }
