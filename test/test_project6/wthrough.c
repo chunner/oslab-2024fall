0339061b2cf530bf5112b1e92158aed37655687c
aihuachun <aihuachun22@mails.ucas.ac.cn>
2024-12-20 12:13:52 +0000

    feat(cache): add write back and write through policies with corresponding test files

---
+++ test/test_project6/wthrough.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static char buff[64] = "page_cache_policy = write through\nwrite_back_freq = 30\n\0";
+
+int main(void)
+{
+    sys_cd("proc/sys");
+    int fd = sys_open("vm", O_RDWR);
+    sys_write(fd, buff, strlen(buff));
+    printf("[page_cache_policy]: change to write through\n");
+
+    sys_close(fd);
+    sys_cd("../..");
+
+    return 0;
+}
