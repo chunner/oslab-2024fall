ff1217aec20ee9213fb64adf28d8c748f699b6bc
aihuachun <aihuachun22@mails.ucas.ac.cn>
2024-12-20 11:39:43 +0000

    feat(fs): enhance read performance measurement by increasing iterations and calculating speedup

--- test/test_project6/rcache.c
+++ test/test_project6/rcache.c
@@ -9,18 +9,24 @@ int main(void)
     sys_touch("rcache.txt");
     int fd = sys_open("rcache.txt", O_RDWR);
 
+    int offset = 0;
     // write 'hello world!' * 10
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 10; i++)
     {
+        sys_lseek(fd, offset, SEEK_SET);
+        offset += 4096;
         sys_write(fd, "hello world!\n", 13);
     }
     uint64_t old_clk;
     uint64_t new_clk;
     // read with cache
     sys_move_cursor(0, 0);
+    offset = 0;
     old_clk = sys_get_tick();
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 10; i++)
     {
+        sys_lseek(fd, offset, SEEK_SET);
+        offset += 4096;
         sys_read(fd, buff, 13);
         for (int j = 0; j < 13; j++)
         {
@@ -28,14 +34,18 @@ int main(void)
         }
     }
     new_clk = sys_get_tick();
-    printf("[read with cache] time: %d\n", new_clk - old_clk);
+    uint64_t t1 = new_clk - old_clk;
+    printf("[read with cache] time: t1 = %d\n", t1);
     // read without cache
     sys_bflush();
     sys_init_bcache();
+    offset = 0;
     old_clk = sys_get_tick();
     sys_lseek(fd, 0, SEEK_SET);
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 10; i++)
     {
+        sys_lseek(fd, offset, SEEK_SET);
+        offset += 4096;
         sys_read(fd, buff, 13);
         for (int j = 0; j < 13; j++)
         {
@@ -43,10 +53,11 @@ int main(void)
         }
     }
     new_clk = sys_get_tick();
-    printf("[read without cache] time: %d\n", new_clk - old_clk);
+    uint64_t t2 = new_clk - old_clk;
+    printf("[read without cache] time: t2 = %d\n", new_clk - old_clk);
 
-
-
+    uint64_t speedup = t2 * 100 / t1;
+    printf("t2/t1 = %d%%\n", speedup);
 
 
     sys_close(fd);
