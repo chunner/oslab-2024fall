11b0b452967df81cb2b7704aae6e65a74eb950ee
aihuachun <aihuachun22@mails.ucas.ac.cn>
2024-12-23 06:10:16 +0000

    feat(test): add cursor movement to write back and write through test files

--- test/test_project6/wthrough.c
+++ test/test_project6/wthrough.c
@@ -6,6 +6,7 @@ static char buff[64] = "page_cache_policy = write through\nwrite_back_freq = 30\
 
 int main(void)
 {
+    sys_move_cursor(0, 0);
     sys_cd("proc/sys");
     int fd = sys_open("vm", O_RDWR);
     sys_write(fd, buff, strlen(buff));
